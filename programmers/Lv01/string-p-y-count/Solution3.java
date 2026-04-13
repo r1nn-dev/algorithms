db440a973702c04b8aa36c0e94ffc575a8be2ee1
R1NN <r1nn650t@gmail.com>
2026-04-13 21:26:03 +0900

    Update Solution3.java

--- programmers/Lv01/string-p-y-count/Solution3.java
+++ programmers/Lv01/string-p-y-count/Solution3.java
@@ -1,12 +1,15 @@
 class Solution {
     boolean solution(String s) {
-        int balance = 0;
+        int count = 0;
 
+				// 문자열 한 글자씩 순회
         for (char c : s.toCharArray()) {
-            if (c == 'p' || c == 'P') balance++;
-            else if (c == 'y' || c == 'Y') balance--;
+		        // 대소문자 직접 비교
+            if (c == 'p' || c == 'P') count++;        // p나 P 이면 -> count 1 증가
+            else if (c == 'y' || c == 'Y') count--;   // y나 Y 이면 -> count 1 감소 
         }
 
-        return balance == 0;
+				// count가 0이면 p와 y 개수가 같다 => true 반환
+        return count == 0;
     }
 }
