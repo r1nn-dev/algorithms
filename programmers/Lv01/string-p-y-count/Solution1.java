6d6d09068627cb43379cae4a5a6bc5cb51ba4e7a
R1NN <r1nn650t@gmail.com>
2026-04-13 19:39:32 +0900

    Create Solution1.java

---
+++ programmers/Lv01/string-p-y-count/Solution1.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean solution(String s) {
+        int pCount = 0;
+        int yCount = 0;
+
+        s = s.toLowerCase();
+
+        for (char c : s.toCharArray()) {
+            if (c == 'p') pCount++;
+            else if (c == 'y') yCount++;
+        }
+
+        return pCount == yCount;
+    }
+}
