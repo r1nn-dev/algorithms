25b8eb9bcd48f0ffe72d0adb0a4027be6b975d17
R1NN <r1nn650t@gmail.com>
2026-04-13 21:25:34 +0900

    Update Solution1.java

--- programmers/Lv01/string-p-y-count/Solution1.java
+++ programmers/Lv01/string-p-y-count/Solution1.java
@@ -3,13 +3,17 @@ class Solution {
         int pCount = 0;
         int yCount = 0;
 
+        // 대소문자 구분 제거
+        // toLowerCase(): 문자열을 모두 소문자로 변환
         s = s.toLowerCase();
 
+        // 문자열을 한 글자씩 전체 순회하며 p, y 개수 카운팅
         for (char c : s.toCharArray()) {
-            if (c == 'p') pCount++;
-            else if (c == 'y') yCount++;
+            if (c == 'p') pCount++;        // p 개수 증가
+            else if (c == 'y') yCount++;   // y 개수 증가
         }
 
+        // p 개수와 y 개수가 같으면 true 반환
         return pCount == yCount;
     }
 }
