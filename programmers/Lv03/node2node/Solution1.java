48d303ba17c7faf63c9dd9eb45d9ee3cb10d871b
R1NN <r1nn650t@gmail.com>
2026-04-27 05:43:35 +0900

    Update Solution1.java

--- programmers/Lv03/node2node/Solution1.java
+++ programmers/Lv03/node2node/Solution1.java
@@ -1,16 +1,17 @@
-// 방법1: 인접 리스트 + BFS
+// Solution1: 인접 리스트 + BFS 방식 
 
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-		    // 그래프 -> 인접 리스트: 1번부터 n번까지 사용하므로 n + 1 크기로 생성
+		// 그래프 -> 인접 리스트: 
+		// 노드 번호가 1번부터 n번까지 사용하므로 n + 1 크기로 생성
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
         
-        // 간선 정보는 양방향이므로 양쪽에 모두 추가
+        // 간선 정보는 양방향이므로 양쪽 리스트에 모두 추가
         for (int[] e : edge) {
             int a = e[0];
             int b = e[1];
@@ -19,7 +20,7 @@ class Solution {
         }
         
         // 각 노드까지의 최단 거리 저장 배열
-        // -1: 아직 방문하지 않았다. 
+        // -1: 아직 방문하지 않은 노드를 의미 
         int[] dist = new int[n + 1];
         Arrays.fill(dist, -1);
         
@@ -30,14 +31,14 @@ class Solution {
         queue.offer(1);
         dist[1] = 0;      // 시작점에서 시작점까지의 거리는 0
         
-        // BFS 시작
+        // BFS 탐색 
         while (!queue.isEmpty()) {
             int current = queue.poll();
             // 현재 노드와 연결된 모든 다음 노드 확인
             for (int next : graph.get(current)) {
                 // 아직 방문하지 않은 노드라면
                 if (dist[next] == -1) {
-		                // 현재 노드 거리 + 1 이 next 노드까지의 최단 거리
+		            // 현재 노드 거리 + 1 이 next 노드까지의 최단 거리
                     dist[next] = dist[current] + 1;
                     // 다음 탐색을 위해 큐에 삽입
                     queue.offer(next);
