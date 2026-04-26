3f020e6898875b7686bbdf89219b68579e2dbf51
R1NN <r1nn650t@gmail.com>
2026-04-27 03:51:03 +0900

    Update Solution1.java

--- programmers/Lv01/digit-sum/Solution1.java
+++ programmers/Lv01/digit-sum/Solution1.java
@@ -4,7 +4,7 @@ class Solution {
 
         // 정수를 문자열로 변환한 뒤, 각 문자를 하나씩 순회
         for (char ch : String.valueOf(n).toCharArray()) {
-            sum += ch - '0'; // 문자 숫자를 실제 정수 값으로 변환
+            sum += ch - '0';   // 문자 숫자를 실제 정수 값으로 변환
         }
 
         return sum;
