7844b8a7c755401ba80ec9b042d20f8bb7753275
R1NN <r1nn650t@gmail.com>
2026-04-27 04:33:10 +0900

    Update Solution2.java

--- programmers/Lv01/string-p-y-count/Solution2.java
+++ programmers/Lv01/string-p-y-count/Solution2.java
@@ -3,6 +3,7 @@ class Solution {
         int count = 0;
         
         // 문자열을 한 글자씩 전체 순회
+        // 대소문자 구분 제거
         // toLowerCase(): 문자열을 모두 소문자로 변환
         for (char c : s.toLowerCase().toCharArray()) {
             if (c == 'p') count++;   // p 이면 -> count 1 증가
