18dbcdc5b23c9541cd2b62a377d5acacb0b77219
R1NN <r1nn650t@gmail.com>
2026-04-27 03:51:30 +0900

    Update Solution2.java

--- programmers/Lv01/digit-sum/Solution2.java
+++ programmers/Lv01/digit-sum/Solution2.java
@@ -1,10 +1,13 @@
 class Solution {
     public int solution(int n) {
         int sum = 0;
+
+        // 일의 자리 숫자를 하나씩 추출하면서 합산
         while (n > 0) {
-            sum += n % 10;  // 일의 자리 추출
-            n /= 10;        // 일의 자리 제거
+            sum += n % 10; // 현재 일의 자리 숫자 추출
+            n /= 10;       // 일의 자리 제거
         }
+
         return sum;
     }
 }
