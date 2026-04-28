9ceae59bf1ef3663624205bce9f095f5ad89fd51
R1NN <r1nn650t@gmail.com>
2026-04-28 13:08:23 +0900

    Create add-numbers/Solution1.java

---
+++ programmers/Lv01/add-numbers/Solution1.java
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len) {
+    int answer = -1;
+    return answer;
+}
