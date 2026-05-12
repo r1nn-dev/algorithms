46e3fc8422bfab91334fd24d75e384211178b05b
R1NN <r1nn650t@gmail.com>
2026-05-12 21:02:07 +0900

    Update Solution1.java

--- programmers/Lv02/correct-parentheses/Solution1.java
+++ programmers/Lv02/correct-parentheses/Solution1.java
@@ -1,12 +1,30 @@
-// Solution1: 
+// Solution1: 카운터 방식 (권장)
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        // 아직 닫히지 않은 '(' 개수를 저장한다.
+        int count = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        // 문자열을 왼쪽부터 한 글자씩 확인한다.
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
-        return answer;
+            // '(' 이면 나중에 닫혀야 할 괄호가 하나 늘어난다.
+            if (ch == '(') {
+                count++;
+            }
+            // ')' 이면 이전에 열린 괄호 하나를 닫는다.
+            else {
+                count--;
+
+                // 닫을 수 있는 '(' 가 없는데 ')' 가 나온 경우이다.
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+
+        // 모든 괄호를 확인한 뒤 열린 괄호가 남아 있지 않아야 한다.
+        return count == 0;
     }
 }
