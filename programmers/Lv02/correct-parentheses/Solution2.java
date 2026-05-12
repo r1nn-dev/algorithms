241f0b1c559bd81adc2aaf2e7b5614ee3b041128
R1NN <r1nn650t@gmail.com>
2026-05-12 21:03:15 +0900

    Update Solution2.java

--- programmers/Lv02/correct-parentheses/Solution2.java
+++ programmers/Lv02/correct-parentheses/Solution2.java
@@ -2,30 +2,31 @@
 
 class Solution {
     boolean solution(String s) {
-        // 아직 닫히지 않은 '(' 를 저장할 Stack 역할의 Deque
+        // 1. 아직 닫히지 않은 '(' 를 저장할 Stack을 만든다.
         Deque<Character> stack = new ArrayDeque<>();
 
-        // 문자열을 왼쪽부터 한 글자씩 확인한다.
+        // 2. 문자열을 왼쪽부터 오른쪽으로 한 글자씩 확인한다.
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            // '(' 이면 나중에 닫혀야 하므로 Stack에 저장한다.
+            // 3. 현재 문자가 '(' 이면 나중에 닫아야 하므로 Stack에 넣는다.
             if (ch == '(') {
                 stack.push(ch);
             }
-            // ')' 이면 이전에 열린 '(' 하나와 짝을 맞춘다.
+
+            // 4. 현재 문자가 ')' 이면 이전에 열린 '(' 하나와 짝을 맞춘다.
             else {
-                // 닫을 수 있는 '(' 가 없으면 올바르지 않은 괄호이다.
+                // 5. Stack이 비어 있으면 닫을 '(' 가 없다는 뜻이므로 false를 반환한다.
                 if (stack.isEmpty()) {
                     return false;
                 }
 
-                // 가장 최근에 열린 '(' 하나를 닫는다.
+                // 6. 가장 최근에 열린 '(' 하나를 Stack에서 꺼낸다.
                 stack.pop();
             }
         }
 
-        // 모든 괄호를 확인한 뒤 Stack이 비어 있어야 한다.
+        // 7. 모든 문자를 확인한 뒤 Stack이 비어 있으면 모든 괄호가 닫힌 것이다.
         return stack.isEmpty();
     }
 }
