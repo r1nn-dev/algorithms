f5baa8663df8f39a80bdfa72d97bd3bd7f1c5fbc
R1NN <r1nn650t@gmail.com>
2026-05-19 13:18:51 +0900

    Update Solution1.java

--- programmers/Lv02/more-spicy/Solution1.java
+++ programmers/Lv02/more-spicy/Solution1.java
@@ -1,8 +1,30 @@
-// Solution1:
+// Solution1: Solution1: PriorityQueue 방식 (권장) 
+
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        return answer;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int value : scoville) {
+            pq.offer(value);
+        }
+        
+        int count = 0;
+        
+        while (pq.peek() < K) {
+            if (pq.size() < 2) {
+                return -1;
+            }
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            int mixed = first + second * 2;
+            
+            pq.offer(mixed);
+            count++;
+        }
+        
+        return count;
     }
 }
