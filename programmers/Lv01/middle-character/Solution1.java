3cf0d3f1006e69e5c534afc51e56095c8d6b4801
R1NN <r1nn650t@gmail.com>
2026-05-19 13:14:18 +0900

    Update Solution1.java

--- programmers/Lv01/middle-character/Solution1.java
+++ programmers/Lv01/middle-character/Solution1.java
@@ -1,8 +1,14 @@
-// Solution1: 
+// Solution1: substring 방식 (권장)
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        int length = s.length();
+        int mid = length / 2;
+        
+        if (length % 2 == 1) {
+            return s.substring(mid, mid + 1);
+        }
+        
+        return s.substring(mid - 1, mid + 1);
     }
 }
