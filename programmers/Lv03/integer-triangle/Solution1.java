78aa5350a0f5665e7f8d600cb191cb3ddd876c99
R1NN <r1nn650t@gmail.com>
2026-05-19 13:22:26 +0900

    Update Solution1.java

--- programmers/Lv03/integer-triangle/Solution1.java
+++ programmers/Lv03/integer-triangle/Solution1.java
@@ -1,8 +1,18 @@
-// Solution1:
+// Solution1: 상향식 DP 방식 (권장)
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        return answer;
+        int n = triangle.length;
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                int left = triangle[i + 1][j];
+                int right = triangle[i + 1][j + 1];
+                
+                triangle[i][j] += Math.max(left, right);
+            }
+        }
+        
+        return triangle[0][0];
     }
 }
