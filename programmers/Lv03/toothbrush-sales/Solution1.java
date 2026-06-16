2d686c9889787f646164f1c318a058ece5aa1fd8
R1NN <r1nn650t@gmail.com>
2026-06-16 23:35:55 +0900

    Create Solution1.java

---
+++ programmers/Lv03/toothbrush-sales/Solution1.java
@@ -0,0 +1,8 @@
+// Solution1: 방식 (권장)
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = {};
+        return answer;
+    }
+}
