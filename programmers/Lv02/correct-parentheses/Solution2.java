bf62eed17126449937dafbb5050734e66917dd02
R1NN <r1nn650t@gmail.com>
2026-07-01 22:02:03 +0900

    Update Solution2.java

--- programmers/Lv02/correct-parentheses/Solution2.java
+++ programmers/Lv02/correct-parentheses/Solution2.java
@@ -2,8 +2,12 @@
 
 class Solution {
     boolean solution(String s) {
+        if (s.length() % 2 == 1) { 
+            return false; 
+        }
+        
         // 1. 아직 닫히지 않은 '(' 를 저장할 Stack을 만든다.
-        Deque<Character> stack = new ArrayDeque<>();
+        Stack<Character> stack = new Stack<>();
 
         // 2. 문자열을 왼쪽부터 오른쪽으로 한 글자씩 확인한다.
         for (int i = 0; i < s.length(); i++) {
@@ -13,14 +17,12 @@ class Solution {
             if (ch == '(') {
                 stack.push(ch);
             }
-
             // 4. 현재 문자가 ')' 이면 이전에 열린 '(' 하나와 짝을 맞춘다.
             else {
                 // 5. Stack이 비어 있으면 닫을 '(' 가 없다는 뜻이므로 false를 반환한다.
                 if (stack.isEmpty()) {
                     return false;
                 }
-
                 // 6. 가장 최근에 열린 '(' 하나를 Stack에서 꺼낸다.
                 stack.pop();
             }
