36ac8b2c22aab240606a3cd750e13edec3309433
R1NN <r1nn650t@gmail.com>
2026-07-03 22:55:01 +0900

    Update Solution1.java

--- programmers/Lv03/immigration-inspection/Solution1.java
+++ programmers/Lv03/immigration-inspection/Solution1.java
@@ -1,7 +1,8 @@
-// Solution1: 이진탐색 기반 Parametric Search
+// Solution1: 이진탐색 기반 Parametric Search 방식 
 
 class Solution {
     public long solution(int n, int[] times) {
+        // 가장 빠른 심사 시간을 저장할 변수
         long minTime = Long.MAX_VALUE;
 
         // 가장 빠른 심사 시간을 찾는다.
@@ -10,15 +11,21 @@ class Solution {
             minTime = Math.min(minTime, time);
         }
 
+        // 정답이 될 수 있는 최소 시간
         long left = 1;
+        // 가장 빠른 심사관이 혼자 n명을 모두 심사하는 시간이 최대 후보가 된다.
         long right = minTime * n;
+        // 가능한 시간 중 최솟값을 저장할 변수
         long answer = right;
 
+        // 시간 범위에 대해 이진탐색을 수행한다.
         while (left <= right) {
             long mid = left + (right - left) / 2;
+
+            // mid분 동안 심사할 수 있는 총 인원 수
             long count = 0;
 
-            // mid분 동안 모든 심사관이 처리할 수 있는 사람 수를 계산한다.
+            // 모든 심사관이 mid분 동안 처리할 수 있는 사람 수를 계산한다.
             for (int time : times) {
                 count += mid / time;
 
@@ -29,18 +36,22 @@ class Solution {
                 }
             }
 
+            // mid분 안에 모든 사람을 심사할 수 있는 경우
             if (count >= n) {
                 // mid분 안에 모든 사람을 심사할 수 있다.
-                // 더 짧은 시간도 가능한지 확인한다.
                 answer = mid;
+                // 더 짧은 시간도 가능한지 확인한다.
                 right = mid - 1;
-            } else {
+            } 
+            // mid분 안에 모든 사람을 심사할 수 없는 경우
+            else {
                 // mid분으로는 모든 사람을 심사할 수 없다.
                 // 더 긴 시간이 필요하다.
                 left = mid + 1;
             }
         }
 
+        // 모든 사람을 심사할 수 있는 최소 시간을 반환한다.
         return answer;
     }
 }
